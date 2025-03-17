9e7c598e351f46aa86b3bd21727025277796b1d7
Gikaria <kelvingikaria154@gmail.com>
2025-03-17 20:33:08 +0000

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(t):
+    for char in t:
+        e = ord(char)
+        if e >= ord('a') or e <= ord('z'):
+            e -= 32
+        print("{}".format(char(e)), end="")
+    print()
